faac5038c39ef07431ece0f69760f6fbf5537ef0
eka prasasti <ekaputraprasasti@gmail.com>
2016-11-26 05:59:43 +0700

    fix try filestack

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var app = express();
